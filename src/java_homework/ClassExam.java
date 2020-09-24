9992158199d448e67f9c942ea9db9683a6817650
DESKTOP-OKRHNV5\USER <wndgn98@naver.com>
2020-09-24 20:07:23 +0900

    자바 프로젝트 연동과제

---
+++ src/java_homework/ClassExam.java
@@ -0,0 +1,25 @@
+package java_homework;
+
+public class ClassExam {
+
+	public static void main(String[] args) {
+		int age, grade, studentnumber;
+		String school, name, major;
+		
+		name = "박중후";
+		school = "한림대학교";
+		age = 23;
+		grade = 2;
+		studentnumber = 20175222;
+		major = "스마트 iot전공";
+		
+		System.out.println("이름 : " + name);
+		System.out.println("학교 : " + school);
+		System.out.println("나이 : " + age);
+		System.out.println("학년 : " + grade);
+		System.out.println("학번 : " + studentnumber);
+		System.out.println("학과 : " + major);
+		
+	}
+
+}
